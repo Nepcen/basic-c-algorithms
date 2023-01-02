c40ab78b5abcedb4e9412f60152eb6044295361e
Yusuf Abacık <yusufabacik@gmail.com>
2023-01-02 15:40:43 +0300

    renamed putNumbers and change funciton name

---
+++ putNumber/putNumber.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void putChar(char c) {
+    write(1, & c, 1);
+}
+
+void putNumbers(int nb) {
+    if (nb == -2147483648)
+        write(1, "-2147483648", 11);
+    else if (nb < 0) {
+        putChar('-');
+        nb = -nb;
+        putNumbers(nb);
+    } else if (nb > 9) {
+        putNumbers(nb / 10);
+        putNumbers(nb % 10);
+    } else
+        putChar(nb + 48);
+}
+
+int main(void) {
+    putNumbers(42);
+    
+    return 0;
+}
